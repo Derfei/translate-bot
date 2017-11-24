f1d4073cbf53fc586e294c8af537edfa534c7165
xin LONG <longyiyuan@outlook.com>
2017-11-24 23:08:37 +0800

    second
    将聊天机器人改为翻译机器人

--- app.js
+++ app.js
@@ -18,7 +18,7 @@
 
 var express = require('express'); // app server
 var bodyParser = require('body-parser'); // parser for post requests
-var Conversation = require('watson-developer-cloud/conversation/v1'); // watson sdk
+var waston = require('watson-developer-cloud'); // watson sdk
 
 var app = express();
 
@@ -27,36 +27,26 @@ app.use(express.static('./public')); // load UI from public folder
 app.use(bodyParser.json());
 
 // Create the service wrapper
-var conversation = new Conversation({
-  // If unspecified here, the CONVERSATION_USERNAME and CONVERSATION_PASSWORD env properties will be checked
-  // After that, the SDK will fall back to the bluemix-provided VCAP_SERVICES environment property
-  //'username': process.env.CONVERSATION_USERNAME,
-  //'password': process.env.CONVERSATION_PASSWORD,
-  'version_date': '2017-05-26'
+var conversation = new waston.LanguageTranslatorV2({
+  url: 'https://gateway.watsonplatform.net/language-translation/api',username: '167e15aa-600b-4103-a8d1-0eedcc5f94ed', password:  'uSjIpVD63pet'
 });
 
 // Endpoint to be call from the client side
 app.post('/api/message', function(req, res) {
-  var workspace = process.env.WORKSPACE_ID || '<workspace-id>';
-  if (!workspace || workspace === '<workspace-id>') {
-    return res.json({
-      'output': {
-        'text': 'The app has not been configured with a <b>WORKSPACE_ID</b> environment variable. Please refer to the ' + '<a href="https://github.com/watson-developer-cloud/conversation-simple">README</a> documentation on how to set this variable. <br>' + 'Once a workspace has been defined the intents may be imported from ' + '<a href="https://github.com/watson-developer-cloud/conversation-simple/blob/master/training/car_workspace.json">here</a> in order to get a working application.'
-      }
-    });
-  }
   var payload = {
-    workspace_id: workspace,
-    context: req.body.context || {},
-    input: req.body.input || {}
+    text: req.body.input,
+    source: 'en',
+    target: 'es'
   };
 
   // Send the input to the conversation service
-  conversation.message(payload, function(err, data) {
+  conversation.translate(payload, function(err, data) {
     if (err) {
-      return res.status(err.code || 500).json(err);
+      return res.json({output: {text: 'hello'}});
+      //return res.status(err.code || 500).json(err);
     }
-    return res.json(updateMessage(payload, data));
+    return res.json({output: {text: 'hello'}}
+    );
   });
 });
 
@@ -67,28 +57,6 @@ app.post('/api/message', function(req, res) {
  * @return {Object}          The response with the updated message
  */
 function updateMessage(input, response) {
-  var responseText = null;
-  if (!response.output) {
-    response.output = {};
-  } else {
-    return response;
-  }
-  if (response.intents && response.intents[0]) {
-    var intent = response.intents[0];
-    // Depending on the confidence of the response the app can return different messages.
-    // The confidence will vary depending on how well the system is trained. The service will always try to assign
-    // a class/intent to the input. If the confidence is low, then it suggests the service is unsure of the
-    // user's intent . In these cases it is usually best to return a disambiguation message
-    // ('I did not understand your intent, please rephrase your question', etc..)
-    if (intent.confidence >= 0.75) {
-      responseText = 'I understood your intent was ' + intent.intent;
-    } else if (intent.confidence >= 0.5) {
-      responseText = 'I think your intent was ' + intent.intent;
-    } else {
-      responseText = 'I did not understand your intent';
-    }
-  }
-  response.output.text = responseText;
   return response;
 }
 
